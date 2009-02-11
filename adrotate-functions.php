48d3efcd2ec9409b95c23b1cadbd0dc0ca60a6ae
SHRIKEE <SHRIKEE@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-11 23:18:52 +0000

    version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/adrotate/tags/2.1@93823 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adrotate-functions.php
@@ -0,0 +1,191 @@
+<?php
+/*-------------------------------------------------------------
+ Name:      adrotate_banner
+
+ Purpose:   Show a banner as requested in the WP Theme or post
+ Receive:   $group_ids, $banner_id, $block, $preview
+ Return:    $output
+-------------------------------------------------------------*/
+function adrotate_banner($group_ids, $banner_id = 0, $block = 0, $preview = false) {
+	global $wpdb;
+
+	if($group_ids) {
+		$group_ids = explode(",", $group_ids);
+		$x = array_rand($group_ids, 1);
+		$now = current_time('timestamp');
+
+		if($block > 0 AND $banner_id < 1) {
+			$limit = $block;
+		} else {
+			$limit = 1;
+		}
+
+		if($banner_id > 0 AND $block < 1) {
+			$select_banner = " AND `id` = '".$banner_id."'";
+		} else {
+			$select_banner = " ORDER BY rand()";
+		}
+
+		if($preview == false) {
+			$active_banner = " `active` = 'yes' AND";
+			$show_banner = "'$now' >= `startshow` AND '$now' <= `endshow` AND ";
+		}
+
+		$SQL = "SELECT `id`, `bannercode`, `image`, `link`, `tracker` FROM `".$wpdb->prefix."adrotate` WHERE ".$show_banner.$active_banner." `group` = '".$group_ids[$x]."' ".$select_banner." LIMIT $limit";
+		if($banners = $wpdb->get_results($SQL)) {
+			$output = '';
+			foreach($banners as $banner) {
+				$banner_output = $banner->bannercode;
+				if($banner->tracker == "Y") {
+					$banner_output = str_replace('%link%', get_option('siteurl').'/wp-content/plugins/adrotate/adrotate-out.php?trackerid='.$banner->id, $banner_output);
+				} else {
+					$banner_output = str_replace('%link%', $banner->link, $banner_output);
+				}
+				$banner_output = str_replace('%image%', get_option('siteurl').'/wp-content/banners/'.$banner->image, $banner_output);
+
+				$output .= $banner_output;
+
+				if($preview == false) {
+					$wpdb->query("UPDATE `".$wpdb->prefix."adrotate` SET `shown` = `shown` + 1 WHERE `id` = '$banner->id'");
+				}
+			}
+		} else {
+			$output = '<!-- The group is empty or the banners in it are disabled/expired! -->';
+		}
+
+	} else {
+		$output = '<span style="color: #F00; font-style: italic; font-weight: bold;">Error, no group_id specified! Check your syntax or contact an administrator!</span>';
+	}
+
+	$output = stripslashes(html_entity_decode($output, ENT_QUOTES));
+
+	echo $output;
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_shortcode
+
+ Purpose:   Show a banner as requested in a post or page using shortcodes
+ Receive:   $atts, $content
+ Return:    adrotate_banner()
+-------------------------------------------------------------*/
+function adrotate_shortcode($atts, $content = null) {
+
+	if(!empty($atts['group'])) $group_ids = $atts['group'];
+		else $group_ids = '';
+	if(!empty($atts['block'])) $block = $atts['block'];
+		else $block = 0;
+	if(!empty($atts['banner'])) $banner_id = $atts['banner'];
+		else $banner_id = 0;
+
+	return adrotate_banner($group_ids, $block, $banner_id, false);
+
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_clean_trackerdata
+
+ Purpose:   Removes old trackerdata
+ Receive:   -none-
+ Return:    -none-
+-------------------------------------------------------------*/
+function adrotate_clean_trackerdata() {
+	global $wpdb;
+
+	if(empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+		$remote_ip = $_SERVER["REMOTE_ADDR"];
+	} else {
+		$remote_ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+	}
+
+	$buffer = explode(',', $remote_ip, 2);
+
+	$removeme = current_time('timestamp') - 86400;
+	$wpdb->query("DELETE FROM `".$wpdb->prefix."adrotate_tracker` WHERE `timer` < ".$removeme." AND `ipaddress` = '$buffer[0]'");
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_send_data
+
+ Purpose:   Register events at meandmymac.net's database
+ Receive:   $action
+ Return:    -none-
+-------------------------------------------------------------*/
+function adrotate_send_data($action) {
+	$adrotate_tracker = get_option('adrotate_tracker');
+
+	// Prepare data
+	$date			= date('U');
+	$plugin			= 'AdRotate';
+	$version		= '2.1';
+	//$action -> pulled from function args
+
+	// User choose anonymous?
+	if($adrotate_tracker['anonymous'] == 'Y') {
+		$ident 		= 'Anonymous';
+		$blogname 	= 'Anonymous';
+		$blogurl	= 'Anonymous';
+		$email		= 'Anonymous';
+	} else {
+		$ident 		= md5(get_option('siteurl'));
+		$blogname	= get_option('blogname');
+		$blogurl	= get_option('siteurl');
+		$email		= get_option('admin_email');
+	}
+
+	// Build array of data
+	$post_data = array (
+		'headers'	=> null,
+		'body'		=> array(
+			'ident'		=> $ident,
+			'blogname' 	=> base64_encode($blogname),
+			'blogurl'	=> base64_encode($blogurl),
+			'email'		=> base64_encode($email),
+			'date'		=> $date,
+			'plugin'	=> $plugin,
+			'version'	=> $version,
+			'action'	=> $action,
+		),
+	);
+
+	// Destination
+	$url = 'http://stats.meandmymac.net/receiver.php';
+
+	wp_remote_post($url, $post_data);
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_check_config
+
+ Purpose:   Create or update the options
+ Receive:   -none-
+ Return:    -none-
+-------------------------------------------------------------*/
+function adrotate_check_config() {
+	if ( !$tracker = get_option('adrotate_tracker') ) {
+		$tracker['register']		 		= 'Y';
+		$tracker['anonymous'] 				= 'N';
+		update_option('adrotate_tracker', $tracker);
+	}
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_options_submit
+
+ Purpose:   Save options from dashboard
+ Receive:   $_POST
+ Return:    -none-
+-------------------------------------------------------------*/
+function adrotate_options_submit() {
+	$buffer = get_option('adrotate_tracker');
+
+	// Prepare Tracker settings
+	if(isset($_POST['adrotate_register'])) $tracker['register'] = 'Y';
+		else $tracker['register'] = 'N';
+	if(isset($_POST['adrotate_anonymous'])) $tracker['anonymous'] = 'Y';
+		else $tracker['anonymous'] = 'N';
+	if($tracker['register'] == 'N' AND $buffer['register'] == 'Y') { adrotate_send_data('Opt-out'); }
+
+	update_option('adrotate_tracker', $tracker);
+}
+?>
---
+++ adrotate-manage.php
@@ -0,0 +1,260 @@
+<?php
+/*-------------------------------------------------------------
+ Name:      adrotate_folder_contents
+
+ Purpose:   List folder contents
+ Receive:   -None-
+ Return:	-None-
+-------------------------------------------------------------*/
+function adrotate_folder_contents($current) {
+	global $wpdb;
+	
+	if ($handle = opendir(ABSPATH.'/wp-content/banners/')) {
+		$output = '';
+	    while (false !== ($file = readdir($handle))) {
+			$fileinfo = pathinfo($file);
+	
+	        if ($file != "." && $file != ".." 
+	        AND (strtolower($fileinfo['extension']) == "jpg" OR strtolower($fileinfo['extension']) == "gif" 
+	        OR strtolower($fileinfo['extension']) == "png" OR strtolower($fileinfo['extension']) == "jpeg"
+	        OR strtolower($fileinfo['extension']) == "swf" OR strtolower($fileinfo['extension']) == "flv")) {
+	            $output .= "<option ";
+	            if($current == $file) { $output .= "selected"; }
+	            $output .= ">".$file."</option>";
+	        }
+	    }
+	    closedir($handle);
+	}
+	
+	return $output;
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_insert_input
+
+ Purpose:   Prepare input form on saving new or updated banners
+ Receive:   -None-
+ Return:	-None-
+-------------------------------------------------------------*/
+function adrotate_insert_input() {
+	global $wpdb, $adrotate_tracker;
+	
+	$banner_id 			= $_POST['adrotate_edit_id'];
+	$author 			= $_POST['adrotate_username'];
+	$title	 			= htmlspecialchars(trim($_POST['adrotate_title'], "\t\n "), ENT_QUOTES);
+	$bannercode			= htmlspecialchars(trim($_POST['adrotate_bannercode'], "\t\n "), ENT_QUOTES);
+	$thetime 			= date('U');
+	$active 			= $_POST['adrotate_active'];
+	$group				= $_POST['adrotate_group'];
+	$image				= $_POST['adrotate_image'];
+	$link				= htmlspecialchars(trim($_POST['adrotate_link'], "\t\n "), ENT_QUOTES);
+	$tracker			= $_POST['adrotate_tracker'];
+	$sday 				= htmlspecialchars(trim($_POST['adrotate_sday'], "\t\n "), ENT_QUOTES);
+	$smonth 			= htmlspecialchars(trim($_POST['adrotate_smonth'], "\t\n "), ENT_QUOTES);
+	$syear 				= htmlspecialchars(trim($_POST['adrotate_syear'], "\t\n "), ENT_QUOTES);
+	$eday 				= htmlspecialchars(trim($_POST['adrotate_eday'], "\t\n "), ENT_QUOTES);
+	$emonth 			= htmlspecialchars(trim($_POST['adrotate_emonth'], "\t\n "), ENT_QUOTES);
+	$eyear 				= htmlspecialchars(trim($_POST['adrotate_eyear'], "\t\n "), ENT_QUOTES);
+
+	
+	if (strlen($title)!=0 AND strlen($bannercode)!=0) {
+		if(strlen($smonth) == 0) 	$smonth 	= date('m');
+		if(strlen($sday) == 0) 		$sday 		= date('d');
+		if(strlen($syear) == 0) 	$syear 		= date('Y');
+		if(strlen($emonth) == 0) 	$emonth 	= $smonth;
+		if(strlen($eday) == 0) 		$eday 		= $sday;
+		if(strlen($eyear) == 0) 	$eyear 		= $syear+1;
+		
+		$startdate 	= gmmktime($shour, $sminute, 0, $smonth, $sday, $syear);
+		$enddate 	= gmmktime($ehour, $eminute, 0, $emonth, $eday, $eyear);
+
+		if(isset($tracker) AND strlen($tracker) != 0) $tracker = 'Y';			
+			else $tracker = 'N';
+
+		/* Check if you need to update or insert a new record */
+		if(strlen($banner_id) != 0) {
+			/* Update */
+			$postquery = "UPDATE 
+			".$wpdb->prefix."adrotate 
+			SET
+			`title` = '$title', `bannercode` = '$bannercode', `updated` = '$thetime', `author` = '$author', 
+			`active` = '$active', `startshow` = '$startdate', `endshow` = '$enddate', `group` = '$group', 
+			`image` = '$image', `link` = '$link', `tracker` = '$tracker'
+			WHERE 
+			`id` = '$banner_id'";
+			$action = "update";
+		} else {
+			/* New */
+			$postquery = "INSERT INTO ".$wpdb->prefix."adrotate
+			(`title`, `bannercode`, `thetime`, `updated`, `author`, `active`, `startshow`, `endshow`, `group`, `image`, `link`, `tracker`, `clicks`, `shown`) 
+			VALUES 
+			('$title', '$bannercode', '$thetime', '$thetime', '$author', '$active', '$startdate', '$enddate', '$group', '$image', '$link', '$tracker', 0, 0)";
+			$action = "new";
+		}
+		if($wpdb->query($postquery) !== FALSE) {
+			if($adrotate_tracker['register'] == 'Y') { 
+				if($action == 'new'){ 
+					adrotate_send_data('New Banner'); 
+				} else {
+					adrotate_send_data('Update Banner');
+				}
+			}
+			adrotate_return($action);
+			exit;
+		} else {
+			die('[MySQL error] '.mysql_error());
+		}
+	} else {
+		adrotate_return('field_error');
+		exit;
+	}
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_insert_group
+
+ Purpose:   Add a new group
+ Receive:   -None-
+ Return:	-None-
+-------------------------------------------------------------*/
+function adrotate_insert_group() {
+	global $wpdb, $adrotate_tracker;
+	
+	$name = $_POST['adrotate_group'];
+	
+	if (strlen($name) != 0) {
+		$postquery = "INSERT INTO ".$wpdb->prefix."adrotate_groups
+		(name)
+		VALUES ('$name')";		
+		$action = "group_new";
+		if($wpdb->query($postquery) !== FALSE) {
+			if($adrotate_tracker['register'] == 'Y') { adrotate_send_data('New Group'); }
+			adrotate_return($action);
+			exit;
+		} else {
+			die(mysql_error());
+		}
+	} else {
+		adrotate_return('group_field_error');
+		exit;
+	}
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_request_delete
+
+ Purpose:   Prepare removal of banner or group from database
+ Receive:   -none-
+ Return:    -none-
+-------------------------------------------------------------*/
+function adrotate_request_delete() {
+	global $userdata, $wpdb, $adrotate_tracker;
+
+	$banner_ids = $_POST['bannercheck'];
+	$group_ids = $_POST['groupcheck'];
+
+	if($banner_ids != '') {
+		foreach($banner_ids as $banner_id) {
+			adrotate_delete($banner_id, 'banner');
+			if($adrotate_tracker['register'] == 'Y') { adrotate_send_data('Delete Banner'); }
+		}
+	}
+	if($group_ids != '') {
+		foreach($group_ids as $group_id) {
+			adrotate_delete($group_id, 'group');
+			if($adrotate_tracker['register'] == 'Y') { adrotate_send_data('Delete Group'); }
+		}
+	}
+	adrotate_return('delete');
+	exit;
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_delete
+
+ Purpose:   Remove banner or group from database
+ Receive:   $id, $what
+ Return:    boolean
+-------------------------------------------------------------*/
+function adrotate_delete($id, $what) {
+	global $wpdb;
+
+	if($id > 0) {
+		if($what == 'banner') {
+			$SQL = "SELECT
+			".$wpdb->prefix."adrotate.author,
+			".$wpdb->prefix."users.display_name as display_name
+			FROM
+			".$wpdb->prefix."adrotate,
+			".$wpdb->prefix."users
+			WHERE
+			".$wpdb->prefix."adrotate.id = '$id'
+			AND
+			".$wpdb->prefix."users.display_name = ".$wpdb->prefix."adrotate.author";
+	
+			$banner = $wpdb->get_row($SQL);
+	
+			if ($banner->display_name == $banner->author ) {
+				$SQL = "DELETE FROM ".$wpdb->prefix."adrotate WHERE id = $id";
+				if($wpdb->query($SQL) == FALSE) {
+					die(mysql_error());
+				}
+			} else {
+				adrotate_return('no_access');
+				exit;
+			}
+		} else if ($what == 'group') {
+			$SQL = "DELETE FROM ".$wpdb->prefix."adrotate_groups WHERE id = $id";
+			if($wpdb->query($SQL) == FALSE) {
+				die(mysql_error());
+			}
+		} else {
+			adrotate_return('error');
+			exit;
+		}
+	}
+}
+
+/*-------------------------------------------------------------
+ Name:      adrotate_return
+
+ Purpose:   Redirect to various pages
+ Receive:   $action
+ Return:    -none-
+-------------------------------------------------------------*/
+function adrotate_return($action) {
+	switch($action) {
+		case "new" :
+			wp_redirect('admin.php?page=adrotate&action=created');
+		break;
+		
+		case "group_new" :
+			wp_redirect('admin.php?page=adrotate2&action=group_new');
+		break;
+		
+		case "update" :
+			wp_redirect('admin.php?page=adrotate2&action=updated');
+		break;
+		
+		case "group_field_error" :
+			wp_redirect('admin.php?page=adrotate&action=group_field_error');
+		break;
+		
+		case "field_error" :
+			wp_redirect('admin.php?page=adrotate&action=field_error');
+		break;
+		
+		case "no_access" :
+			wp_redirect('admin.php?page=adrotate2&action=no_access');
+		break;
+		
+		case "delete" :
+			wp_redirect('admin.php?page=adrotate2&action=deleted');
+		break;
+		
+		case "error" :
+			wp_redirect('admin.php?page=adrotate2&action=error');
+		break;
+	}
+}
+?>
