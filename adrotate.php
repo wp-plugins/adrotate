170e01f8adde8705422f0a8f69664b70f37b426e
SHRIKEE <SHRIKEE@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-15 23:33:02 +0000

    Updated read me for 3.3
    git-svn-id: https://plugins.svn.wordpress.org/adrotate/trunk@476160 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adrotate.php
+++ adrotate.php
@@ -4,7 +4,7 @@ Plugin Name: AdRotate
 Plugin URI: http://www.adrotateplugin.com
 Description: The very best and most convenient way to publish your ads.
 Author: Arnan de Gans
-Version: 3.6.7
+Version: 3.6.8
 Author URI: http://meandmymac.net/
 License: GPL2
 */
