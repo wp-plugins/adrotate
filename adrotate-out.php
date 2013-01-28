c1a8b729f1063aa2bade517675952f6fc3f4340f
adegans <adegans@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-28 14:04:52 +0000

    Version 3.8.3.2
    git-svn-id: https://plugins.svn.wordpress.org/adrotate/tags/3.8.3.2@660155 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adrotate-out.php
@@ -0,0 +1,63 @@
+<?php
+/*  
+Copyright 2010-2013 Arnan de Gans - AJdG Solutions (email : info@ajdg.net)
+*/
+
+/*-------------------------------------------------------------
+ Purpose:   Facilitate outgoing affiliate links
+ Receive:   $_GET
+ Return:	-None-
+ Since:		2.0
+-------------------------------------------------------------*/
+define('WP_USE_THEMES', false);
+require('../../../wp-blog-header.php');
+
+global $wpdb, $adrotate_crawlers, $adrotate_debug;
+
+if(isset($_GET['track']) OR $_GET['track'] != '') {
+	if($adrotate_debug['track'] == true) {
+		$meta = $_GET['track'];
+	} else {
+		$meta = base64_decode($_GET['track']);
+	}
+	$useragent 	= trim($_SERVER['HTTP_USER_AGENT'], ' \t\r\n\0\x0B');
+	$prefix		= $wpdb->prefix;
+
+	if(isset($_GET['preview'])) $preview = $_GET['preview'];
+		else $preview = 0;	
+	list($ad, $group, $block, $bannerurl) = explode(",", $meta);
+
+	$remote_ip 	= adrotate_get_remote_ip();
+	$now 		= time();
+	$today 		= gmmktime(0, 0, 0, gmdate("n"), gmdate("j"), gmdate("Y"));
+	if($adrotate_debug['timers'] == true) {
+		$tomorrow = $now;
+	} else {
+		$tomorrow = $now + 86400;
+	}
+		
+	if($bannerurl) {
+		if(is_array($adrotate_crawlers)) $crawlers = $adrotate_crawlers;
+			else $crawlers = array();
+	
+		$nocrawler = true;
+		foreach ($crawlers as $crawler) {
+			if (preg_match("/$crawler/i", $useragent)) $nocrawler = false;
+		}
+
+		$ip = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM `".$prefix."adrotate_tracker` WHERE `ipaddress` = '%s' AND `stat` = 'c' AND `timer` < '$tomorrow' AND `bannerid` = %d LIMIT 1;", $remote_ip, $ad));
+		if($ip < 1 AND $nocrawler == true AND $preview == 0 AND !empty($useragent)) {
+			$wpdb->query($wpdb->prepare("UPDATE `".$prefix."adrotate_stats` SET `clicks` = `clicks` + 1 WHERE `ad` = %d AND `group` = %d AND `block` = %d AND `thetime` = '$today';", $ad, $group, $block));
+			if($remote_ip != "unknown" AND $remote_ip != "") {
+				$wpdb->insert($prefix.'adrotate_tracker', array('ipaddress' => $remote_ip, 'timer' => $now, 'bannerid' => $ad, 'stat' => 'c', 'useragent' => $useragent));
+			}
+		}
+		wp_redirect(htmlspecialchars_decode($bannerurl), 302);
+		exit();
+	} else {
+		echo 'There was an error retrieving the ad! Contact an administrator!';
+	}
+} else {
+	echo 'No or invalid Ad ID specified! Contact an administrator!';
+}
+?>
