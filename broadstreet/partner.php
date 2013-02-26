0be0c8f4e22d2ea046445929da2e06629704392c
adegans <adegans@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-26 03:33:08 +0000

    Version 3.8.4
    git-svn-id: https://plugins.svn.wordpress.org/adrotate/tags/library@673318 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ broadstreet/partner.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Changing things here breaks things!
+ */
+
+define('BROADSTREET_PARTNER_NAME', 'AJdGSolutions');
+define('BROADSTRET_PARTNER_PLUGIN', '/adrotate');
+define('BROADSTREET_VENDOR_PATH', '/library/broadstreet');
+define('BROADSTREET_AD_TAG_SELECTOR', '[name="adrotate_bannercode"]');
+define('BROADSTREET_DEBUG', false);
+?>
